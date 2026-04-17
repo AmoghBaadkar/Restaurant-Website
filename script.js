d77b3d3d9d1761ad98f17b06be9ec11302f58f37
AmoghBaadkar <122138698+AmoghBaadkar@users.noreply.github.com>
2026-04-17 22:39:34 +0530

    Initial upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function scrollToMenu() {
+  document.querySelector("#menu").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+window.addEventListener("scroll", function () {
+  const nav = document.getElementById("navbar");
+
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
